28b5af1e6a2d78d9ef7aad10945713a66f954047
P. Gomersbach <p.gomersbach@rely.nl>
2017-08-28 19:48:43 +0000

    add notice

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@
 #
 class profile_mssql::params {
   $monitor_address = $::monitor_address
-
+  notice("Level = $::productionlevel")
   case $::productionlevel {
     'production': {
       $backup_dir       = undef
