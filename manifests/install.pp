f9a46294942dc3bee3daa827886e9f9622c649b3
P. Gomersbach <p.gomersbach@rely.nl>
2017-08-30 10:36:29 +0000

    debug msg

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,7 +8,9 @@ class profile_mssql::install {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  notice("Level = ${productionlevel}")
+  notice("Level = ${::productionlevel}")
+  notice("Level = ${profile_mssql::productionlevel}")
+  notice("Level = ${::profile_mssql::productionlevel}")
   # install network time sync
   include 'winntp'
 
