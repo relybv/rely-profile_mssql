ad5b82f444f31f4ed1514f38b0537d6fd728a0b8
P. Gomersbach <p.gomersbach@rely.nl>
2017-08-30 12:26:02 +0000

    debug

--- manifests/install.pp
+++ manifests/install.pp
@@ -27,23 +27,23 @@ class profile_mssql::install {
       target         => 'c:/windows/temp/',
       targetfilename => 'mssql.iso',
       overwrite      => true,
-      before         => Class['::sqlserver'],
+#      before         => Class['::sqlserver'],
     }
   }
 
   # install sql server
-  class { '::sqlserver':
-    backup_dir       => $::profile_mssql::params::backup_dir,
-    database_dir     => $::profile_mssql::params::database_dir,
-    database_log_dir => $::profile_mssql::params::database_log_dir,
-    edition          => $::profile_mssql::params::edition,
-    features         => $::profile_mssql::params::features,
-    force_english    => $::profile_mssql::params::force_english,
-    instance_dir     => $::profile_mssql::params::instance_dir,
-    instance_name    => $::profile_mssql::params::instance_name,
-    license          => $::profile_mssql::params::license,
-    license_type     => $::profile_mssql::params::license_type,
-    sa_password      => $::profile_mssql::params::sa_password,
-    source           => $::profile_mssql::params::source,
-  }
+#  class { '::sqlserver':
+#    backup_dir       => $::profile_mssql::params::backup_dir,
+#    database_dir     => $::profile_mssql::params::database_dir,
+#    database_log_dir => $::profile_mssql::params::database_log_dir,
+#    edition          => $::profile_mssql::params::edition,
+#    features         => $::profile_mssql::params::features,
+#    force_english    => $::profile_mssql::params::force_english,
+#    instance_dir     => $::profile_mssql::params::instance_dir,
+#    instance_name    => $::profile_mssql::params::instance_name,
+#    license          => $::profile_mssql::params::license,
+#    license_type     => $::profile_mssql::params::license_type,
+#    sa_password      => $::profile_mssql::params::sa_password,
+#    source           => $::profile_mssql::params::source,
+#  }
 }
