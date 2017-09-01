a4385d3b0f9093e67aeb1e550a3e00a5fd714c2c
P. Gomersbach <p.gomersbach@rely.nl>
2017-09-01 10:02:00 +0000

    add debug

--- manifests/install.pp
+++ manifests/install.pp
@@ -63,7 +63,7 @@ class profile_mssql::install {
   }
 
   exec { 'install_mssql2012':
-    command   => "${profile_mssql::media}\\setup.exe /Action=Install /IACCEPTSQLSERVERLICENSETERMS /Q /HIDECONSOLE /CONFIGURATIONFILE=C:\\sql2012install.ini /SAPWD=\"${::profile_mssql::sapwd}\" /SQLSVCPASSWORD=\"${::profile_mssql::sqlsvcpassword}\" /AGTSVCPASSWORD=\"${::profile_mssql::agtsvcpassword}\" /ASSVCPASSWORD=\"${::profile_mssql::assvcpassword}\" /RSSVCPASSWORD=\"${::profile_mssql::rssvcpassword}\"",
+    command   => "${profile_mssql::media}\\setup.exe /Action=Install /IACCEPTSQLSERVERLICENSETERMS /QS /HIDECONSOLE /CONFIGURATIONFILE=C:\\sql2012install.ini /SAPWD=\"${::profile_mssql::sapwd}\" /SQLSVCPASSWORD=\"${::profile_mssql::sqlsvcpassword}\" /AGTSVCPASSWORD=\"${::profile_mssql::agtsvcpassword}\" /ASSVCPASSWORD=\"${::profile_mssql::assvcpassword}\" /RSSVCPASSWORD=\"${::profile_mssql::rssvcpassword}\"",
     cwd       => $profile_mssql::media,
     path      => $profile_mssql::media,
     logoutput => true,
